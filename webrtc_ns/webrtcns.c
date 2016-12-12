70de86552e1d222a0847285da528e0981ff618c7
zhang.zheng <zhang.zhang@immomo.com>
2016-12-12 12:06:56 +0800

    webrtcns

---
+++ webrtc_ns/webrtcns.c
@@ -0,0 +1,8 @@
+//
+//  webrtcns.c
+//
+//  Created by Jc on 16/12/12.
+//  Copyright © 2016年 jichuan. All rights reserved.
+//
+
+#include "webrtcns.h"
---
+++ webrtc_ns/webrtcns.h
@@ -0,0 +1,13 @@
+//
+//  webrtcns.h
+//
+//  Created by Jc on 16/12/12.
+//  Copyright © 2016年 jichuan. All rights reserved.
+//
+
+#ifndef webrtcns_h
+#define webrtcns_h
+
+#include <stdio.h>
+
+#endif /* webrtcns_h */
